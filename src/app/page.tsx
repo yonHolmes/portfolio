6e7ab7ac8480c92f365d822d8162ecda23bff84c
Yon Holmes <yon.holmes@gmail.com>
2025-02-08 20:38:31 +0000

    fix: Missed the leading slash

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <main className={styles.main}>
         <Image
           className={styles.logo}
-          src="portfolio/next.svg"
+          src="/portfolio/next.svg"
           alt="Next.js logo"
           width={180}
           height={38}
@@ -29,7 +29,7 @@ export default function Home() {
           >
             <Image
               className={styles.logo}
-              src="portfolio/vercel.svg"
+              src="/portfolio/vercel.svg"
               alt="Vercel logomark"
               width={20}
               height={20}
@@ -54,7 +54,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="portfolio/file.svg"
+            src="/portfolio/file.svg"
             alt="File icon"
             width={16}
             height={16}
@@ -68,7 +68,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="portfolio/window.svg"
+            src="/portfolio/window.svg"
             alt="Window icon"
             width={16}
             height={16}
@@ -82,7 +82,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="portfolio/globe.svg"
+            src="/portfolio/globe.svg"
             alt="Globe icon"
             width={16}
             height={16}
