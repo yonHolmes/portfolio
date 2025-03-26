ab2aa0ed1841d795273528412735266f7a9b757f
Yon <Yon.Holmes@gmail.com>
2025-03-26 21:25:31 +0000

    chore: Add Robots and Verification to Meta Data

--- src/components/elements/app/App.tsx
+++ src/components/elements/app/App.tsx
@@ -16,6 +16,23 @@ import Link from 'next/link';
 export const metadata: Metadata = {
   title: "Yon Holmes",
   description: "Gamer, turned Developer",
+  robots: {
+    index: false,
+    follow: false,
+    nocache: false,
+    googleBot: {
+      index: false,
+      follow: false,
+      noimageindex: false,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  verification: {
+    google: 'psRtEERv2Cf55bV9e9jj63Rr5nff5heKae8MZXFxbnI',
+  },
+  // <meta name="google-site-verification" content="psRtEERv2Cf55bV9e9jj63Rr5nff5heKae8MZXFxbnI" />
 };
 
 const routes = [
