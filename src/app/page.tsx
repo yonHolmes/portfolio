cbfa72e3db67a8347149f92cc890d019b819b19e
Yon <Yon.Holmes@gmail.com>
2025-03-26 21:56:31 +0000

    chore: Move Metadata to route page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,27 @@
 import { Home } from "@/components/pages/home/Home";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Yon Holmes",
+  description: "Gamer, turned Developer",
+  robots: {
+    index: false,
+    follow: false,
+    nocache: false,
+    googleBot: {
+      index: false,
+      follow: false,
+      noimageindex: false,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  verification: {
+    google: 'psRtEERv2Cf55bV9e9jj63Rr5nff5heKae8MZXFxbnI',
+  },
+  // <meta name="google-site-verification" content="psRtEERv2Cf55bV9e9jj63Rr5nff5heKae8MZXFxbnI" />
+};
 
 export default function HomeRoute() {
   console.log('HomeRoute');
--- src/components/elements/app/App.tsx
+++ src/components/elements/app/App.tsx
@@ -10,30 +10,7 @@ import { useBreakpoints } from "@/context/useBreakpoints";
 import { AppSidebar } from "@/components/elements/appSidebar/AppSidebar";
 import { ApplicationBar } from "../applicationBar/ApplicationBar";
 import { navRoutes } from "@/consts/navigationConsts";
-import { Metadata } from "next";
 import Link from 'next/link';
-
-export const metadata: Metadata = {
-  title: "Yon Holmes",
-  description: "Gamer, turned Developer",
-  robots: {
-    index: false,
-    follow: false,
-    nocache: false,
-    googleBot: {
-      index: false,
-      follow: false,
-      noimageindex: false,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
-    },
-  },
-  verification: {
-    google: 'psRtEERv2Cf55bV9e9jj63Rr5nff5heKae8MZXFxbnI',
-  },
-  // <meta name="google-site-verification" content="psRtEERv2Cf55bV9e9jj63Rr5nff5heKae8MZXFxbnI" />
-};
 
 const routes = [
   {
