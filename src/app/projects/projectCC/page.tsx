1cb0a67a3db1a8ea4ea01ef67c5654c5fc703a56
Yon <Yon.Holmes@gmail.com>
2025-03-27 20:51:02 +0000

    chore: Note to self find a pre-commit hook for renaming files and folders

--- src/app/projects/projectCC/page.tsx
+++ src/app/projects/projectCC/page.tsx
@@ -1,4 +1,4 @@
-import { ProjectCC } from "@/components/pages/projects/projectCC/ProjectCC";
+import { ProjectCC } from "@/components/pages/projects/ProjectCCpage/ProjectCC";
 
 
 export default function RouteProjectsCC() {
